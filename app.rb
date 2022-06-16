28440455bbba145452b9975ad17f1de9337f17f5
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-16 15:50:49 +0300

    app and main classs created

---
+++ app.rb
@@ -0,0 +1,8 @@
+require_relative './student'
+require_relative './classroom'
+require_relative './book'
+require_relative './rental'
+require_relative './person'
+
+class App
+end 
---
+++ main.rb
@@ -0,0 +1,16 @@
+def menu
+    print "Welcome to School Library App! \n\n 
+    Please Choose an option by entering a number: 
+     1. List all books
+     2. List all people
+     3. Create a perosn
+     4. Create a book
+     5. Create a rental
+     6. List all rentals for a given person id
+     7. Exit\n"
+end
+
+def main
+    menu
+
+end 
